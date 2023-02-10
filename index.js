440ebbe16213fa6bd1b32a885ec2e0a0a3409387
Nikita Mikheev <nikita.miheev.1999@mail.ru>
2023-02-10 19:14:29 +0300

    fix

--- index.js
+++ index.js
@@ -49,67 +49,67 @@ let file = args[2];
 
 // ЗАДАЧА 5. Парсер csv файла в json. Работает 
 
-// function cvsParse(file) {
-//     let cvsString = readFileSync('testExcel.csv', {encoding: 'utf8'}); // получаем строки
+function cvsParse(file) {
+    let cvsString = readFileSync('testExcel.csv', {encoding: 'utf8'}); // получаем строки
 
-//     let cvsArr = cvsString.split(/\r\n|\r|\n/);
-//     let arr = [];
-//     for (let index = 0; index < cvsArr.length; index++) {
-//         arr.push(cvsArr[index].split(';'));
-//     }
-//     console.log(arr);
-//     let arrayJSON = [];
-//     let minusLength = -1;
-//     if(arr.length>=arr[0].length) {
-//       minusLength = arr.length - arr[0].length;
-//   }
+    let cvsArr = cvsString.split(/\r\n|\r|\n/);
+    let arr = [];
+    for (let index = 0; index < cvsArr.length; index++) {
+        arr.push(cvsArr[index].split(';'));
+    }
+    console.log(arr);
+    let arrayJSON = [];
+    let minusLength = -1;
+    if(arr.length>=arr[0].length) {
+      minusLength = arr.length - arr[0].length;
+  }
   
-//     for (let index = 0; index < arr.length-minusLength; index++) {
-//         let count = index;
-//         for (let i = 1; i < arr.length-1; i++) {
+    for (let index = 0; index < arr.length-minusLength; index++) {
+        let count = index;
+        for (let i = 1; i < arr.length-1; i++) {
 
-//             let key = arr[0][count];
-//             let value = arr[i][count];
-//             arrayJSON.push({[key] : value});
-//         }
-//     }
-//     console.log(arrayJSON);
-//     let strJSON = JSON.stringify(arrayJSON);
-//     writeFileSync(file + '.json', strJSON); // !!! НАЗВАНИЕ СОЗДАВАЕМОГО ФАЙЛА ПЕРЕДАЕМ ПАРАМЕТРОМ КОНСОЛИ !!! */
-//     return strJSON;
-// }
+            let key = arr[0][count];
+            let value = arr[i][count];
+            arrayJSON.push({[key] : value});
+        }
+    }
+    console.log(arrayJSON);
+    let strJSON = JSON.stringify(arrayJSON);
+    writeFileSync(file + '.json', strJSON); // !!! НАЗВАНИЕ СОЗДАВАЕМОГО ФАЙЛА ПЕРЕДАЕМ ПАРАМЕТРОМ КОНСОЛИ !!! */
+    return strJSON;
+}
 
-// let backToObj = cvsParse(file);
-
-
+let backToObj = cvsParse(file);
 
 
 
-// rewriteAndCheckJSON(backToObj,file); //ЗАДАЧА 6. Проверка файла JSON и добавления поля. ПРОДОЛЖЕНИЕ ЗАДАЧИ 5. Теперь созданный файл мы проверяем на верный формат, добавляем в объекты поля, и парсим снова в JSON
-// function rewriteAndCheckJSON(backToObj, file) {
-//     let objFromJSON;
-//     try {
-//         try {
-//             objFromJSON = JSON.parse(backToObj);
-//         }    
+
+
+rewriteAndCheckJSON(backToObj,file); //ЗАДАЧА 6. Проверка файла JSON и добавления поля. ПРОДОЛЖЕНИЕ ЗАДАЧИ 5. Теперь созданный файл мы проверяем на верный формат, добавляем в объекты поля, и парсим снова в JSON
+function rewriteAndCheckJSON(backToObj, file) {
+    let objFromJSON;
+    try {
+        try {
+            objFromJSON = JSON.parse(backToObj);
+        }    
         
-//         catch(error) {
-//             throw new Error('Данные не в формате JSON!')
-//         }
-//         for (let index = 0; index < objFromJSON.length; index++){
-//         let key = 'new';
-//         let value = 'value';
-//         objFromJSON[index][key] = value;
-//         }
+        catch(error) {
+            throw new Error('Данные не в формате JSON!')
+        }
+        for (let index = 0; index < objFromJSON.length; index++){
+        let key = 'new';
+        let value = 'value';
+        objFromJSON[index][key] = value;
+        }
 
-//         let secondJSON = JSON.stringify(objFromJSON);
+        let secondJSON = JSON.stringify(objFromJSON);
 
-//         writeFileSync(file + 'New.json', secondJSON); //создаст файл с таким же названием и прибавит New
-//     }
-//     catch(error) {
-//         throw new Error('Ошибка выполнения! Не удаётся перезаписать файл');
-//     }    
-// }
+        writeFileSync(file + 'New.json', secondJSON); //создаст файл с таким же названием и прибавит New
+    }
+    catch(error) {
+        throw new Error('Ошибка выполнения! Не удаётся перезаписать файл');
+    }    
+}
 
 
 
